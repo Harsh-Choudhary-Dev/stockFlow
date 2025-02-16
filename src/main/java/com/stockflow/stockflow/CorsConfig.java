3c4e6c516151c12e92a96eef0abff907530a5dbb
Harsh <harshchoudhary50226@gmail.com>
2025-02-16 23:41:43 +0530

    First commit.

---
+++ src/main/java/com/stockflow/stockflow/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.stockflow.stockflow;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Apply CORS for all API endpoints
+                        .allowedOrigins("https://yummy-sibel-stockflow123-be085e06.koyeb.app") // Allowed frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
